64bdc356da77d48d387399950ee0eb9a9f22154e
Koba <zkt.hobby@gmail.com>
2023-07-30 16:30:25 +0900

    Gemの追加、インスタンスの定義　記述

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  
+
+  # describe 'ユーザー新規登録' do
+  #   it 'nicknameが空では登録できない' do
+  #     user = User.new(nickname: '', email: 'test@example', password: 'abc123', password_confirmation: 'abc123')
+  #     binding.pry
+  #     user.valid?
+  #   end
+  #   it 'emailが空では登録できない' do
+  #     # emailが空では登録できないテストコードを記述します
+  #   end
+  # end
+
+
+
+
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "nickname,email,password,last_name,fist_name,last_name_kana,first_name_kana,birthdayが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できない場合' do
+      it "nameが空では登録できない" do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+      it "emailが空では登録できない" do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it "passwordが空では登録できない" do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '12345'
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it 'passwordが129文字以上では登録できない' do
+        @user.password =  Faker::Internet.password(min_length: 129, max_length: 150)
+        @user.password_confirmation =  @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it '重複したemailが存在する場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+
+     it 'emailは@を含まないと登録できない' do
+        @user.email = 'testmail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+    end
+  end
+
+
+
+end
