5df67f0e86d7ffbe5063445e24d98c5e4373b9dc
Koba <zkt.hobby@gmail.com>
2023-08-05 13:24:36 +0900

    ActiveHas実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,23 @@
 class Item < ApplicationRecord
 
 
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :shipment_date
+
+
 
   belongs_to :user
   # has_one :shopping_log
 
   has_one_attached :image
-  
+
+ 
+
 end
