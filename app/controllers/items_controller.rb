1f5bfc07c39677c77841a2fdda1c035f921121fe
Koba <zkt.hobby@gmail.com>
2023-08-07 21:56:29 +0900

    商品一覧表示機能　実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :destroy]
 
   def index
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
