2202d022cf467a1db14697c2093174f13553126c
Koba <zkt.hobby@gmail.com>
2023-08-11 10:19:03 +0900

    商品情報編集機能　実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -23,7 +24,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  
+  def edit
+    @item = Item.find(params[:id])
+
+    user_check
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    user_check
+
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
   
 
 
@@ -32,4 +49,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :content, :price, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :shipment_date_id, :image).merge(user_id: current_user.id) 
   end
+
+  def user_check 
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
 end
