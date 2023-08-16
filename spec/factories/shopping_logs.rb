8649736f7f117910d1df3ef13e521687b1cf4f38
Koba <zkt.hobby@gmail.com>
2023-08-16 14:39:48 +0900

    不要な記述の削除

--- spec/factories/shopping_logs.rb
+++ spec/factories/shopping_logs.rb
@@ -1,8 +1,6 @@
 FactoryBot.define do
   factory :shopping_log do
 
-    association :user
-    association :item
 
   end
 end
--- spec/models/shopping_log_spec.rb
+++ spec/models/shopping_log_spec.rb
@@ -1,28 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe ShoppingLog, type: :model do
-  before do
-    @shopping_log = FactoryBot.build(:shopping_log)
-  end
 
-  # context '内容に問題ない場合' do
-  #   it "priceとtokenがあれば保存ができること" do
-  #     binding.pry
-  #     expect(@shopping_log).to be_valid
-  #   end
-  # end
-
-  # context '内容に問題がある場合' do
-  #   it "priceが空では保存ができないこと" do
-  #     @shopping_log.price = nil
-  #     @shopping_log.valid?
-  #     expect(@shopping_log.errors.full_messages).to include("Price can't be blank")
-  #   end
-
-  #   it "tokenが空では登録できないこと" do
-  #     @shopping_log.token = nil
-  #     @shopping_log.valid?
-  #     expect(@shopping_log.errors.full_messages).to include("Token can't be blank")
-  #   end
-  # end
 end
